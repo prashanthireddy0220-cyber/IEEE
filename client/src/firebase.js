95008032c3793f716b0f157757bebcd25e25d59c
prashu05744 <prashanthireddy0220@gmail.com>
2026-08-01 00:11:24 +0530

    updated production API URL

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-
+console.log(import.meta.env);
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
